f34c0255dc25ce5681012a2e4ec0052b14510032
zx24student4280-dotcom <zx24student4280@colegiovalledelmiro.es>
2025-10-15 09:54:17 +0200

    Versión 3: funcionalidad de multiplicar

--- calculadora.py
+++ calculadora.py
@@ -4,5 +4,9 @@ def sumar(a, b):
 def restar(a, b):
     return a - b
 
+def multiplicar(a, b):
+    return a * b
+
 print("Suma:", sumar(2, 3))
 print("Resta:", restar(5, 2))
+print("Multiplicación:", multiplicar(3, 4))
