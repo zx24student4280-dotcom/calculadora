7c82b4301bc5002aaa0971268b8c069feb9be80e
zx24student4280-dotcom <zx24student4280@colegiovalledelmiro.es>
2025-10-15 10:20:58 +0200

    Funcionalidad de dividir añadida

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,16 @@
 def sumar(a, b):
     return a + b
 
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    return a / b
+
 print("Suma:", sumar(2, 3))
+print("Resta:", restar(5, 2))
+print("Multiplicación:", multiplicar(3, 4))
+print("Dividir:", dividir(10, 2))
