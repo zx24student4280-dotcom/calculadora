9ac36773eac60c3ffbad46da8d7f028280255a7c
zx24student4280-dotcom <zx24student4280@colegiovalledelmiro.es>
2025-10-24 13:07:22 +0200

    feat: crear fichero index.html base

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,8 @@
-def sumar(a, b):
-    return a + b
+def sumar(*args):
+    """Suma una cantidad variable de números."""
+    return sum(args)
+
+
 
 def restar(a, b):
     return a - b
@@ -10,7 +13,8 @@ def multiplicar(a, b):
 def dividir(a, b):
     return a / b
 
-print("Suma:", sumar(2, 3))
+print("Suma de 2 y 3:", sumar(2, 3))
+print("Suma de 1, 2, 3, 4:", sumar(1, 2, 3, 4))
 print("Resta:", restar(5, 2))
 print("Multiplicación:", multiplicar(3, 4))
 print("Dividir:", dividir(10, 2))
